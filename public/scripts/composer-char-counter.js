cae08f2cde1021a766d82d4936fd8ebf25f01d63
Jelani Sanders <jelanisands@gmail.com>
2023-06-13 22:38:02 -0400

    remove const tweetText and counter from global

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,20 +1,18 @@
 /* eslint-disable no-undef */
-const $tweetText = $('#tweet-text');
-const $counter = $('.counter');
-
 
 $(function() {
+  const $tweetText = $('#tweet-text');
+  const $counter = $('.counter');
   const maxCharLength = 140;
   $tweetText.on('input', function() {
     const inputLength = $(this).val().length;
     const counter = maxCharLength - inputLength;
-
     $counter.text(counter);
 
     if (counter < 0) {
       $counter.addClass('red-class');
     }
-    if (counter > 0) {
+    if (counter >= 0) {
       $counter.removeClass('red-class');
     }
   });
